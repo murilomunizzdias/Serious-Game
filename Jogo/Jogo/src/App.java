d0f38774944944effa52aa1120404ce1cbef3a82
Murilo Muniz <murilomunizdiaspc@gmail.com>
2025-11-20 01:41:33 -0300

    Add files via upload
    feito ate o video 3 do tutorial

---
+++ Jogo/Jogo/src/App.java
@@ -0,0 +1,19 @@
+import javax.swing.JFrame;
+public class App {
+    public static void main(String[] args) throws Exception {
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setTitle("Unravel");
+
+        GamePanel gamePanel= new GamePanel();
+        window.add(gamePanel);
+
+        window.pack();//so we can see it
+
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+
+        gamePanel.starGameThread();
+    }
+}
