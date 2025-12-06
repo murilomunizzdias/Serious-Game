b99b74457072f44169a2c85ae4470202257692f4
Murilo Muniz <murilomunizdiaspc@gmail.com>
2025-12-06 15:37:55 -0300

    Add files via upload
    OBJECTS

---
+++ Jogo/src/centro/AssetSetter.java
@@ -0,0 +1,20 @@
+package centro;
+import object.OBJ_KEY;
+
+public class AssetSetter {
+    GamePanel gp;
+
+    public AssetSetter(GamePanel gp){
+        this.gp=gp;
+    }
+
+    public void setObject(){
+        gp.obj[0]= new OBJ_KEY();
+        gp.obj[0].worldX=23*gp.tileSize;
+        gp.obj[0].worldY=7*gp.tileSize;
+
+        gp.obj[1]= new OBJ_KEY();
+        gp.obj[1].worldX=20*gp.tileSize;
+        gp.obj[1].worldY=26*gp.tileSize;    
+    }
+}
